271d701beeed2f11128c1e214aefa7986092af5e
crayfishigo <evanyang1120@163.com>
2021-12-14 15:17:07 +0800

    fix doc issue

--- src/lib.rs
+++ src/lib.rs
@@ -1,19 +1,21 @@
 pub mod authenticator;
 
-//!
-//! # Example
-//! ```rust
-//! use crate::authenticator_rs::authenticator;
-//! fn main(){
-//!     let secret =  authenticator::create_secret(32); // create a random secret
-//!     let pin_code_rs = authenticator::current_pin_code(secret.as_str(), 6);
-//!     match pin_code_rs {
-//!             Ok(code) => { println!("Current Pin Code: {}", code) }
-//!             Err(e) => { println!("Something has error: {}", e) }
-//!         }
-//! }
-//!
-//! ```
+/// This Rust crate can be used to interact with the Google Authenticator mobile app for 2-factor-authentication.
+/// This Rust crates can generate secrets, generate codes, validate codes and present a QR-Code for scanning the secret.
+/// It implements TOTP according to RFC6238
+/// # Example
+/// ```rust
+/// use crate::authenticator_rs::authenticator;
+/// fn main(){
+///     let secret =  authenticator::create_secret(32); // create a random secret
+///     let pin_code_rs = authenticator::current_pin_code(secret.as_str(), 6);
+///     match pin_code_rs {
+///             Ok(code) => { println!("Current Pin Code: {}", code) }
+///             Err(e) => { println!("Something has error: {}", e) }
+///         }
+/// }
+///
+/// ```
 
 
 #[cfg(test)]
