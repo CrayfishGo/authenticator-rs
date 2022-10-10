569e03dc1204950f9838b85bc5dd101a9c5bc7c7
crayfishigo <evanyang1120@163.com>
2022-10-10 19:09:00 +0800

    fix build error

--- src/authenticator.rs
+++ src/authenticator.rs
@@ -14,7 +14,7 @@ const MAX_PINCODE_LENGTH: u16 = 9;
 
 const SECRET_MAX_LEN: usize = 128;
 const SECRET_MIN_LEN: usize = 16;
-
+//                               0  1    2     3     4      5       6         7          8          9
 const DIGITS_POWER: [i32; 10] = [1, 10, 100, 1000, 10000, 100000, 1000000, 10000000, 100000000, 1000000000];
 
 /// The cropto signer
@@ -24,20 +24,18 @@ pub trait Signer {
 
 impl Signer for Key {
     fn sign(&self, data: &[u8]) -> Vec<u8> {
-        let mut output = Vec::new();
         let tag = hmac::sign(&self, data);
-        output = tag.as_ref().to_vec();
-        output
+        tag.as_ref().to_vec()
     }
 }
 
 /// create a random secret
 pub fn create_secret(length: u8) -> String {
-    let mut secret = Vec::<char>::new();
+    let mut secret = Vec::<&char>::new();
     let mut index: usize;
     for _ in 0..length {
         index = (rand::random::<u8>() & 0x1F) as usize;
-        secret.push(ALPHABET[index]);
+        secret.push(&ALPHABET[index]);
     }
     secret.into_iter().collect()
 }
@@ -51,7 +49,7 @@ pub fn current_pin_code(secret: &str, code_len: u16) -> Result<String, String> {
     let result = base32_decode(secret);
     match result {
         Ok(key) => {
-            let authenticator = Authenticator::new(get_signer(key.as_slice()), code_len)?;
+            let authenticator = Authenticator::new(get_signer(key.as_slice()), code_len as usize)?;
             Ok(authenticator.generate_response_code(otp_state)?)
         }
         Err(e) => { Err(e) }
@@ -91,7 +89,7 @@ pub fn verify_pin_code(secret: &str, pin_code: &str, code_len: u16) -> bool {
 }
 
 fn get_signer(secret_key: &[u8]) -> Box<dyn Signer> {
-    Box::new(hmac::Key::new(HMAC_SHA1_FOR_LEGACY_USE_ONLY, secret_key))
+    Box::new(Key::new(HMAC_SHA1_FOR_LEGACY_USE_ONLY, secret_key))
 }
 
 fn base32_decode(secret: &str) -> Result<Vec<u8>, String> {
@@ -101,15 +99,15 @@ fn base32_decode(secret: &str) -> Result<Vec<u8>, String> {
     }
 }
 
-#[derive(Debug)]
 pub struct Authenticator {
-    code_len: u16,
+    code_len: usize,
     signer: Box<dyn Signer>,
 }
 
 impl Authenticator {
-    pub fn new(signer: Box<dyn Signer>, code_len: u16) -> Result<Self, String> {
-        if code_len <= 0 || code_len > MAX_PINCODE_LENGTH {
+    pub fn new(signer: Box<dyn Signer>, code_len: usize) -> Result<Self, String> {
+        format!("numer is {}", 10);
+        if code_len <= 0 || code_len > MAX_PINCODE_LENGTH.into() {
             return Err(format!("PinCode Length must be between 1 and {} digits", MAX_PINCODE_LENGTH));
         }
         Ok(Authenticator { code_len, signer })
@@ -123,9 +121,9 @@ impl Authenticator {
         truncated_hash.copy_from_slice(&hash[offset as usize..(offset + 4) as usize]);
         let mut code = i32::from_be_bytes(truncated_hash);
         code &= 0x7FFFFFFF;
-        code %= DIGITS_POWER[self.code_len as usize];
+        code %= DIGITS_POWER[self.code_len];
         let mut code_str = code.to_string();
-        for i in 0..(self.code_len as usize - code_str.len()) {
+        for i in 0..(&self.code_len - code_str.len()) {
             code_str.insert(i, '0');
         }
         Ok(code_str)
